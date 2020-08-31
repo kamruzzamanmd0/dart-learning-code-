9d9acd73d853fceaf31f515bea531664916d378f
kamruzzamanmd <kamruzzamanmd@hotmail.com>
2020-08-31 20:03:04 +0600

    lecture-01

---
+++ lectuer-01.dart
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+
+
+
+
+void main(){
+//   int sum =add(3,4);
+//   print(sum);
+//   print(add(3,5));
+}
+
+int add(int a ,int b)=>a+b;
+
+
+
+
+
+
+// void main(){
+//   String statement= manager(qnty:5,unit:'pcs',item:'pants');
+// print(statement); 
+// }
+
+// manager({int qnty,String unit,String item}){
+//  String statement='$qnty $unit of $item are  is ready' ;
+  
+//   return statement;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// void main(){
+//   manager(qnty:5,unit:'pcs',item:'pants');
+// }
+
+// manager({int qnty,String unit,String item}){
+//   print (qnty);
+//   print('$qnty $unit of $item is ready' );
+// }
+
+
+
+
+
+
+
+
+
+// void main(){
+//   manager(5,'pcs','pants');
+// }
+
+// manager(int qnty,String unit,String item){
+//   print (qnty);
+//   print('$qnty $unit of $item is ready' );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//  add(3,4);
+// }
+//  add(int a, int b){
+//   print(a+b);
+//  }
+
+
+// int a=234;
+// string b='343';
+// double c=3.3;
